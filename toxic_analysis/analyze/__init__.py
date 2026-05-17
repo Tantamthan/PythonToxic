6529e9f4801e11fd1b17a2b19483674519399a2b
Tantamthan <hoangngocnhattan01042005@gmail.com>
2026-05-17 12:40:33 +0700

    last end

--- toxic_analysis/analyze/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-__init__.py
------------
-Package analyze: Thống kê và trực quan hóa.
-"""
--- toxic_analysis/test_script.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys.path.insert(0, '.')
-from collect.youtube_scraper import thu_thap_youtube
-df = thu_thap_youtube(['Akj12zQniWw'], output_path='./data/collected/test_youtube.csv')
-print('So binh luan load duoc:', len(df))
